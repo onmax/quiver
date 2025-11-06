23b9eeeb60c8d162fb655b7050a62352e76d2a31
onmax <maximogarciamtnez@gmail.com>
2025-11-06 18:54:50 +0100

    add app.vue w/ NuxtLayout

--- app/app.vue
+++ app/app.vue
@@ -1,32 +1,8 @@
-<script setup lang="ts">
-useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: 'white' },
-  ],
-  link: [
-    { rel: 'icon', href: '/logo-color.svg' },
-  ],
-  htmlAttrs: {
-    lang: 'en',
-  },
-})
-
-useSeoMeta({
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/landing-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/landing-light.png',
-  twitterCard: 'summary_large_image',
-})
-</script>
-
 <template>
-  <UApp :toaster="{ expand: false }">
-    <AppHeader />
-
-    <UMain>
+  <UApp>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
       <NuxtPage />
-    </UMain>
-
-    <AppFooter />
+    </NuxtLayout>
   </UApp>
 </template>
