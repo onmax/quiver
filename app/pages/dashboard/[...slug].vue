418658b8dd6fcf15000493b9eda39dda7bb0b193
onmax <maximogarciamtnez@gmail.com>
2025-11-06 18:47:38 +0100

    fix: use UDashboardPanel structure

--- app/pages/dashboard/[...slug].vue
+++ app/pages/dashboard/[...slug].vue
@@ -7,11 +7,19 @@ const path = computed(() => route.path)
 
 <template>
   <UDashboardPanel>
-    <UDashboardSection>
+    <template #header>
+      <UDashboardNavbar :title="path">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+      </UDashboardNavbar>
+    </template>
+
+    <template #body>
       <div class="flex flex-col items-center justify-center min-h-[60vh] gap-4">
         <h1 class="text-4xl font-bold">{{ path }}</h1>
         <p class="text-muted">This page is under construction</p>
       </div>
-    </UDashboardSection>
+    </template>
   </UDashboardPanel>
 </template>
